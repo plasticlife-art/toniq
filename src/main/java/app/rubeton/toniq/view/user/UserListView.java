964a43cd4e783d582f6ec25f36efc7a6d100388c
plasticlife-art <plastic.life.ln@gmail.com>
2026-05-04 18:33:10 +0200

    init

---
+++ src/main/java/app/rubeton/toniq/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package app.rubeton.toniq.view.user;
+
+import app.rubeton.toniq.entity.User;
+import app.rubeton.toniq.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
